1505e3869f74e8b8cec54a09aa6ca58cc3da1ae8
crutchM <sads1100@gmail.com>
2021-07-01 15:15:51 +0500

    Initial commit

---
+++ main.py
@@ -0,0 +1,15 @@
+
+from telebot import TeleBot
+
+bot = TeleBot('1706939990:AAEQ2KZ4VRbincT7Sa9TvaL-FRJ7SiD6Z08')
+
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_hi('PyCharm')
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
